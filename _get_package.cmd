41186d0a4a1026406e347ec6b9c63551528277ee
Abyss Morgan <14877700+AbyssMorgan@users.noreply.github.com>
2022-10-29 08:51:05 +0200

    Add license to next package

--- _get_package.cmd
+++ _get_package.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET PATH=%PATH%;%CD%\bin
 IF EXIST "%CD%\AVE-PHP.7z" DEL /Q /A "%CD%\AVE-PHP.7z"
-7z a -mx9 -t7z "%CD%\AVE-PHP.7z" "bin" "includes" "meta" "config" "AVE-PHP.cmd" -x!"config\user.ini"
+7z a -mx9 -t7z "%CD%\AVE-PHP.7z" "bin" "includes" "meta" "config" "AVE-PHP.cmd" -x!"config\user.ini" "LICENSE"
 PAUSE
