64023b36a856bbaf1fbcc9bc6f02af36b0de528c
Abyss Morgan <14877700+AbyssMorgan@users.noreply.github.com>
2022-11-06 15:46:03 +0100

    Update _get_package.cmd

--- _get_package.cmd
+++ _get_package.cmd
@@ -3,5 +3,5 @@ SET PATH=%PATH%;%CD%\bin\main;%CD%\bin\php;%CD%\bin\imagick
 php "%CD%\includes\main.php" --put-version
 php "%CD%\includes\main.php" --guard-generate
 IF EXIST "%CD%\AVE-PHP.7z" DEL /Q /A "%CD%\AVE-PHP.7z"
-7z a -mx9 -t7z "%CD%\AVE-PHP.7z" "bin" "includes" "meta" "config" "AVE-PHP.cmd" "AVE.ave-guard" -x!"config\user.ini" "LICENSE"
+7z a -mx9 -t7z "%CD%\AVE-PHP.7z" "bin" "includes" "meta" "config" "AVE-PHP.cmd" "AVE.ave-guard" "README.md" "LICENSE" -x!"config\user.ini"
 PAUSE
