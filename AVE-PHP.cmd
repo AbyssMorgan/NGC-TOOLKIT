276bf30886f35d63767776969fa9c33b1ef0f1bb
Abyss Morgan <14877700+AbyssMorgan@users.noreply.github.com>
2022-11-14 08:52:23 +0100

    Update AVE-PHP.cmd

--- AVE-PHP.cmd
+++ AVE-PHP.cmd
@@ -3,5 +3,5 @@ chcp 65001
 CLS
 SET PATH=%PATH%;%CD%\bin\main;%CD%\bin\php;%CD%\bin\imagick
 COLOR 9F
-php "%CD%\includes\main.php" --interactive
+"%CD%\bin\php\php.exe" "%CD%\includes\main.php" --interactive
 GOTO :eof
