097fd8fd11a87f9f09d575955b674d2b157abd1d
Abyss Morgan <14877700+AbyssMorgan@users.noreply.github.com>
2023-05-28 15:09:26 +0200

    Update CheckFileIntegrity.php

--- includes/tools/CheckFileIntegrity.php
+++ includes/tools/CheckFileIntegrity.php
@@ -57,34 +57,31 @@ class CheckFileIntegrity {
 		$this->ave->set_subtool("CreatePattern");
 
 		set_input:
-		echo " Input (Folder): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Input (Folder): ");
 		if($line == '#') return false;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_input;
 		$input = $folders[0];
 
 		if(!file_exists($input) || !is_dir($input)){
-			echo " Invalid input folder\r\n";
+			$this->ave->echo(" Invalid input folder");
 			goto set_input;
 		}
 
 		set_output:
-		echo " Output (Folder): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Output (Folder): ");
 		if($line == '#') return false;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_output;
 		$output = $folders[0];
 
 		if((file_exists($output) && !is_dir($output)) || !$this->ave->mkdir($output)){
-			echo " Invalid output folder\r\n";
+			$this->ave->echo(" Invalid output folder");
 			goto set_output;
 		}
 
 		set_name:
-		echo " Name: ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Name: ");
 		if($line == '#') return false;
 
 		$pattern_file = $this->ave->get_folders($line);
@@ -95,30 +92,26 @@ class CheckFileIntegrity {
 		$pattern->setInput($input);
 
 		set_folders:
-		echo " Folders: ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Folders: ");
 		if($line == '#') return false;
 		foreach($this->ave->get_folders($line) as $folder){
 			$pattern->addFolders(str_replace([$input.DIRECTORY_SEPARATOR, $input], "", $folder));
 		}
 
 		if(!empty($line)){
-			echo " More (Y/N): ";
-			$line = $this->ave->get_input();
+			$line = $this->ave->get_input(" More (Y/N): ");
 			if(strtoupper($line[0] ?? 'N') == 'Y') goto set_folders;
 		}
 
 		set_files:
-		echo " Files: ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Files: ");
 		if($line == '#') return false;
 		foreach($this->ave->get_folders($line) as $file){
 			$pattern->addFiles(str_replace([$input.DIRECTORY_SEPARATOR, $input], "", $file));
 		}
 
 		if(!empty($line)){
-			echo " More (Y/N): ";
-			$line = $this->ave->get_input();
+			$line = $this->ave->get_input(" More (Y/N): ");
 			if(strtoupper($line[0] ?? 'N') == 'Y') goto set_files;
 		}
 
@@ -138,15 +131,14 @@ class CheckFileIntegrity {
 		$this->ave->set_subtool("GuardGenerate");
 
 		set_pattern:
-		echo " Pattern (.ave-pat): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Pattern (.ave-pat): ");
 		if($line == '#') return false;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_pattern;
 		$pattern_file = $folders[0];
 
 		if(!file_exists($pattern_file)){
-			echo " Pattern file not exists\r\n";
+			$this->ave->echo(" Pattern file not exists");
 			goto set_pattern;
 		}
 
@@ -155,19 +147,19 @@ class CheckFileIntegrity {
 
 		$input = $pattern->getInput();
 		if(!file_exists($input) || !is_dir($input)){
-			echo " Invalid input folder: \"$input\"\r\n";
+			$this->ave->echo(" Invalid input folder: \"$input\"");
 			goto set_pattern;
 		}
 
 		$files = count($pattern->getFiles());
 		$folders = count($pattern->getFolders());
-		echo " Loaded $folders folders and $files files\r\n";
+		$this->ave->echo(" Loaded $folders folders and $files files");
 
 		$guard_file = str_replace(chr(0x5C).chr(0x5C), chr(0x5C), $this->ave->get_file_path("$input/".pathinfo($pattern_file, PATHINFO_FILENAME).".ave-guard"));
 
 		$cwd = getcwd();
 		chdir($input);
-		echo " Generate $guard_file\r\n";
+		$this->ave->echo(" Generate $guard_file");
 		$guard = new GuardDriver($guard_file, $pattern->getFolders(), $pattern->getFiles());
 		$guard->generate();
 		chdir($cwd);
@@ -184,21 +176,20 @@ class CheckFileIntegrity {
 		$this->ave->set_subtool("CheckIntegrity");
 
 		set_guard:
-		echo " Guard (.ave-guard): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Guard (.ave-guard): ");
 		if($line == '#') return false;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_guard;
 		$guard_file = $folders[0];
 
 		if(!file_exists($guard_file)){
-			echo " Guard file not exists\r\n";
+			$this->ave->echo(" Guard file not exists");
 			goto set_guard;
 		}
 
 		$ini = new IniFile($guard_file, true);
 		if(is_null($ini->get('keys'))){
-			echo " File don't contain one of required information (Not a valid guard file ?)\r\n";
+			$this->ave->echo(" File don't contain one of required information (Not a valid guard file ?)");
 			goto set_guard;
 		}
 
@@ -206,7 +197,7 @@ class CheckFileIntegrity {
 
 		$cwd = getcwd();
 		chdir($input);
-		echo " Validate files from $guard_file\r\n";
+		$this->ave->echo(" Validate files from $guard_file");
 		$guard = new GuardDriver($guard_file, $ini->get('folders_to_scan'), $ini->get('files_to_scan'));
 		$validation = $guard->validate();
 		chdir($cwd);
@@ -242,11 +233,11 @@ class CheckFileIntegrity {
 		}
 
 		$this->ave->write_data("State: $damaged damaged, $missing missing, $unknown unknown");
-		$this->ave->write_data("\r\nDamaged:");
+		$this->ave->write_data(["", "Damaged:"]);
 		$this->ave->write_data($errors['damaged'] ?? []);
-		$this->ave->write_data("\r\nMissing:");
+		$this->ave->write_data(["", "Missing:"]);
 		$this->ave->write_data($errors['missing'] ?? []);
-		$this->ave->write_data("\r\nUnknown:");
+		$this->ave->write_data(["", "Unknown:"]);
 		$this->ave->write_data($errors['unknown'] ?? []);
 
 		$this->ave->open_logs(false);
@@ -259,21 +250,20 @@ class CheckFileIntegrity {
 		$this->ave->set_subtool("GetFilesTree");
 
 		set_guard:
-		echo " Guard (.ave-guard): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Guard (.ave-guard): ");
 		if($line == '#') return false;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_guard;
 		$guard_file = $folders[0];
 
 		if(!file_exists($guard_file)){
-			echo " Guard file not exists\r\n";
+			$this->ave->echo(" Guard file not exists");
 			goto set_guard;
 		}
 
 		$ini = new IniFile($guard_file, true);
 		if(is_null($ini->get('keys'))){
-			echo " File don't contain one of required information (Not a valid guard file ?)\r\n";
+			$this->ave->echo(" File don't contain one of required information (Not a valid guard file ?)");
 			goto set_guard;
 		}
 
@@ -335,21 +325,20 @@ class CheckFileIntegrity {
 
 	public function ToolGuardSetFile() : ?string {
 		set_guard:
-		echo " Guard (.ave-guard): ";
-		$line = $this->ave->get_input();
+		$line = $this->ave->get_input(" Guard (.ave-guard): ");
 		if($line == '#') return null;
 		$folders = $this->ave->get_folders($line);
 		if(!isset($folders[0])) goto set_guard;
 		$guard_file = $folders[0];
 
 		if(!file_exists($guard_file)){
-			echo " Guard file not exists\r\n";
+			$this->ave->echo(" Guard file not exists");
 			goto set_guard;
 		}
 
 		$ini = new IniFile($guard_file, true);
 		if(is_null($ini->get('keys'))){
-			echo " File don't contain one of required information (Not a valid guard file ?)\r\n";
+			$this->ave->echo(" File don't contain one of required information (Not a valid guard file ?)");
 			goto set_guard;
 		}
 
@@ -361,7 +350,7 @@ class CheckFileIntegrity {
 		$input = pathinfo($guard_file, PATHINFO_DIRNAME);
 		$cwd = getcwd();
 		chdir($input);
-		echo " Validate files from $guard_file\r\n";
+		$this->ave->echo(" Validate files from $guard_file");
 		$guard = new GuardDriver($guard_file, $ini->get('folders_to_scan'), $ini->get('files_to_scan'));
 		$validation = $guard->validate(['damaged' => false, 'unknown' => true, 'missing' => true]);
 
